3315fe9ea108ed108ecad81d1fc77146a7a91cf9
Snakeeater97 <14RO40550353@campus.economicas.uba.ar>
2023-06-13 20:07:43 -0300

    Test

--- ProyectoCaiV01/Modulos/Validaciones.cs
+++ ProyectoCaiV01/Modulos/Validaciones.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoCaiV01.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace ProyectoCaiV01.Modulos
 {
     internal class Validaciones
     {
-    }
+        static List<Vuelo> TEST;
+    } 
 }
